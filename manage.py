88d3b7dcbb953b499f59154e184d9c2dda8bd696
Niklas Sirch <sirniklax@gmail.com>
2023-06-18 14:43:20 +0200

    fix name in license

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# SPDX-FileCopyrightText: 2023 2023, Nicolas Bota, Marcel Geiger, Florian Paul, Rajbir Singh, Niklas Sirch, Jan Swiridow, Duc Minh Vu, Mike WegelE
+# SPDX-FileCopyrightText: 2023 2023, Nicolas Bota, Marcel Geiger, Florian Paul, Rajbir Singh, Niklas Sirch, Jan Swiridow, Duc Minh Vu, Mike Wegele
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
